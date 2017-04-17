b2f1f4956cb816eee218a6fd111b6f9358572736
Welcome <Welcome@user>
2017-04-17 22:50:48 +0530

    commit

---
+++ public_html/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".nav-tabs a").click(function(){
+        $(this).tab('show');
+    });
+    $('.nav-tabs a').on('shown.bs.tab', function(event){
+        var x = $(event.target).text();         // active tab
+        var y = $(event.relatedTarget).text();  // previous tab
+       
+
+
+    });
+});
